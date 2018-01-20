1ea7ad8f8d5f82148c1b977b3e83670fe4812a50
Maxwell Koo <mjkoo90@gmail.com>
2018-01-19 20:12:04 -0800

    Listen to bounty events

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"log"
+	"net/url"
 	"os"
 	"time"
 
 	"github.com/dutchcoders/go-clamd"
-
+	"github.com/gorilla/websocket"
 	"github.com/ipfs/go-ipfs-api"
+	"github.com/polyswarm/polyswarm/bounty"
 )
 
 const MAX_DATA_SIZE = 50 * 1024 * 1024
 
-func connectToClamd(url string) (*clamd.Clamd, error) {
-	ret := clamd.NewClamd(url)
+func connectToClamd(host string) (*clamd.Clamd, error) {
+	u := url.URL{Scheme: "tcp", Host: host}
+	ret := clamd.NewClamd(u.String())
 
 	timeout := time.After(60 * time.Second)
 	tick := time.Tick(time.Second)
@@ -31,39 +35,76 @@ func connectToClamd(url string) (*clamd.Clamd, error) {
 	}
 }
 
+func connectToIpfs(host string) (*shell.Shell, error) {
+	ret := shell.NewShell(host)
+
+	timeout := time.After(60 * time.Second)
+	tick := time.Tick(time.Second)
+
+	for {
+		select {
+		case <-timeout:
+			return nil, errors.New("timeout waiting for clamd")
+		case <-tick:
+			if ret.IsUp() {
+				return ret, nil
+			}
+		}
+	}
+}
+
+func connectToPolyswarm(host string) (*websocket.Conn, error) {
+	u := url.URL{Scheme: "ws", Host: host, Path: "/events"}
+	log.Println(u.String())
+
+	timeout := time.After(60 * time.Second)
+	tick := time.Tick(time.Second)
+
+	for {
+		select {
+		case <-timeout:
+			return nil, errors.New("timeout waiting for polyswarm")
+		case <-tick:
+			conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+			if err == nil {
+				return conn, nil
+			}
+		}
+	}
+}
+
 func main() {
 	time.Sleep(15 * time.Second)
 	log.Println("Starting microengine")
 
-	ipfssh := shell.NewShell(os.Getenv("IPFS_HOST"))
-	if ipfssh == nil {
-		log.Fatalln("could not connect to ipfs")
-	}
-	log.Println(ipfssh)
-
-	ipfs_uri := "QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme"
-	//	stat, err := ipfssh.ObjectStat(ipfs_uri)
-	//	log.Println(stat)
-	//	if err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//
-	//	if stat.DataSize > MAX_DATA_SIZE || stat.NumLinks == 0 {
-	//		log.Fatalln("invalid artifact at uri")
-	//	}
-
-	r, err := ipfssh.Cat(ipfs_uri)
-	defer r.Close()
-
-	c, err := connectToClamd(os.Getenv("CLAMD_HOST"))
+	clam, err := connectToClamd(os.Getenv("CLAMD_HOST"))
 	if err != nil {
 		log.Fatalln(err)
 	}
+	_ = clam
 
-	//reader := bytes.NewReader(clamd.EICAR)
-	response, err := c.ScanStream(r, make(chan bool))
-
-	for s := range response {
-		log.Printf("%v %v\n", s, err)
+	ipfssh, err := connectToIpfs(os.Getenv("IPFS_HOST"))
+	if err != nil {
+		log.Fatalln(err)
 	}
+	_ = ipfssh
+
+	conn, err := connectToPolyswarm(os.Getenv("POLYSWARM_HOST"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("error reading from websocket:", err)
+		}
+
+		var event bounty.Event
+		json.Unmarshal(message, &event)
+
+		log.Println("recv:", event)
+	}
+
 }
