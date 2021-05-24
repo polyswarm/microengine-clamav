ec3ed1dfc7f808bff7453fa372941bd0d2944d53
Rob Lathrop <rl@polyswarm.io>
2021-05-24 10:30:10 -0400

    Updates in mwp

--- setup.py
+++ setup.py
@@ -6,26 +6,49 @@ try:
 except IOError:
     long_description = ""
 
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
 setup(
     name="microengineclamav",
     version="0.1.0",
-    description="Clamav microengine",
+    description="Clamav Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     author="PolySwarm Developers",
-    packages=find_packages('src'),
-    package_dir={'': 'src/'},
+    author_email="info@polyswarm.io",
+    url='https://github.com/polyswarm/microengine-clamav',
     install_requires=[
         "celery",
+        "clamd",
         "Flask",
+        "polyswarm-artifact",
         "requests",
         "python-json-logger"
     ],
-    long_description=long_description,
+    packages=find_packages('src'),
+    package_dir={'': 'src/'},
+    python_requires='>=3.6.5,<4',
+    extras_require={
+        "tests": [
+                'pytest',
+                'pytest-cov',
+                'pytest-mock',
+                'requests-mock'
+        ],
+    },
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ]
 )
