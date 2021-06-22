61e6697bc159a7a4a4bf0cc21095de2fac59f5f3
Robert Lathrop <Rizato@users.noreply.github.com>
2021-06-22 16:28:03 -0400

    Add values for HTTPProxyServer (#5)
    * Add squid proxy to compose
    
    * Add squid proxy values
    
    * Sync with mwp
    
    * Fix test imports
    
    * More sync
    
    * Add debug log in handler
    
    * Remove e2e
    
    * Don't start clamav
    
    * Fix tests
    
    * Use task logging
    
    * More logs
    
    * Remove logger
    
    * Set rabbit password
    
    * Fix CLAMD_HOST value
    
    * Only add Proxy env if host present

--- src/microengineclamav/tasks.py
+++ src/microengineclamav/tasks.py
@@ -1,19 +1,24 @@
 from celery import Celery
 
-from microengineclamav.models import Bounty, ScanResult, Verdict, Assertion
+from microengineclamav.models import Bounty, ScanResult, Verdict, Assertion, Phase
 from microengineclamav import settings
 from microengineclamav.scan import scan, compute_bid
 
 celery_app = Celery('tasks', broker=settings.BROKER)
 
-
 @celery_app.task
 def handle_bounty(bounty):
     bounty = Bounty(**bounty)
     scan_result = scan(bounty)
-    bid = None
 
-    if scan_result.verdict == Verdict.MALICIOUS or scan_result.verdict == Verdict.BENIGN:
-        bid = compute_bid(bounty, scan_result)
+    if bounty.phase == Phase.ARBITRATION:
+        scan_response = scan_result.to_vote()
+    else:
+        if scan_result.verdict in [Verdict.UNKNOWN, Verdict.SUSPICIOUS]:
+            # These results don't bid any NCT.
+            bid = 0
+        else:
+            bid = compute_bid(bounty, scan_result)
+        scan_response = scan_result.to_assertion(bid)
 
-    bounty.post_assertion(scan_result.to_assertion(bid))
+    bounty.post_response(scan_response)
