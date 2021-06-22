6cf2d31d8a66e62071910b2d9decc26b0b7a5a60
Rob Lathrop <rl@polyswarm.io>
2021-06-22 14:25:14 -0400

    Remove logger

--- src/microengineclamav/tasks.py
+++ src/microengineclamav/tasks.py
@@ -1,19 +1,15 @@
 from celery import Celery
-from celery.utils.log import get_task_logger
 
 from microengineclamav.models import Bounty, ScanResult, Verdict, Assertion, Phase
 from microengineclamav import settings
 from microengineclamav.scan import scan, compute_bid
 
 celery_app = Celery('tasks', broker=settings.BROKER)
-logger = get_task_logger(__name__)
 
 @celery_app.task
 def handle_bounty(bounty):
     bounty = Bounty(**bounty)
-    logger.debug('Got bounty %s', bounty)
     scan_result = scan(bounty)
-    logger.debug('Bounty %s got ScanResult %s', bounty, scan_result)
 
     if bounty.phase == Phase.ARBITRATION:
         scan_response = scan_result.to_vote()
