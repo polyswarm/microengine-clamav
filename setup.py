ba4377a42a4adc80282e0e61d0b97281ab6ef527
Rob Lathrop <rl@polyswarm.io>
2021-07-30 11:06:19 -0400

    Fix version mismatch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except IOError:
 
 setup(
     name="microengineclamav",
-    version="0.1.0",
+    version="0.2.0",
     description="Clamav Engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
